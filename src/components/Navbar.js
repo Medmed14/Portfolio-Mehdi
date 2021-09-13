2a7c485264d392bd59dca7d8bc451bcdc598cd82
MehdiMemmi <mehdi@dsply.fr>
2021-09-13 19:25:49 +0200

    navbar done

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,6 +1,8 @@
 import React from 'react';
 import myLogo from '../M.png';
-
+// fontawesome icons imports
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faBars } from '@fortawesome/free-solid-svg-icons';
 
 const Navbar = () => {
     
@@ -10,7 +12,7 @@ const Navbar = () => {
   <div className="container-fluid">
     <a className="navbar-brand" href="#"><img className="logo" src={myLogo} alt="M logo"/></a>
     <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-      <span className="navbar-toggler-icon"></span>
+      <FontAwesomeIcon icon={faBars} className="hamburger-btn"/>
     </button>
     <div className="collapse navbar-collapse" id="navbarSupportedContent">
       <ul className="navbar-nav ms-auto mb-2 mb-lg-0 ml-auto">
