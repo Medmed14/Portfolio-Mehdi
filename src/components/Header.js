275ef88813ddfe99c22fe17bebb85e92ebc4cdf7
MehdiMemmi <mehdi@dsply.fr>
2021-09-14 16:26:29 +0200

    <header done>

---
+++ src/components/Header.js
@@ -0,0 +1,23 @@
+import React from 'react';
+import Typed from 'react-typed';
+
+
+
+const Header = () => {
+    return (
+        <div className="header-wraper">
+            <div className="main-info">
+                <h1>Bienvenue, je suis</h1>
+                <Typed 
+                    className="typed-text"
+                    strings={["un développeur web front-end", "très curieux", "un dev créatif", "un React lover","un développeur web back-end", "un passionné d'informatique", "sociable et coopératif", "un autodidacte", "hey, vous êtes encore là? :)" ]}
+                    typeSpeed={60}
+                    backSpeed={50}
+                    loop
+                />
+            </div>
+        </div>
+    )
+}
+
+export default Header
